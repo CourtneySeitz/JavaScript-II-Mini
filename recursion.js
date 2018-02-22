592a3e30ad65936d882c1891c942ebbce9b0b78b
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2018-02-22 15:37:42 -0500

    working on problem 2

--- recursion.js
+++ recursion.js
@@ -1,23 +1,23 @@
 // to test these problems you can run 'node recursion.js' in your terminal
 // Problem 1:
 
-// let n = 1;
-// while (n <= 10) {
-//   console.log('While Loop', n);
-//   n++;
-// }
+ let n = 1;
+ while (n <= 10) {
+   console.log('While Loop', n);
+   n++;
+ }
 // const loop = num => {
 //     if (num > 10) return;
 //     console.log(num);
 //     loop(++num);
 //   };
-  
+
 //  loop(1);
 //   console.log(loop);
 
 
 const countToTen = n => {
-if (n > 10) return; 
+if (n > 10) return;
 console.log('While Loop', n);
 countToTen(++n);
 };
@@ -27,22 +27,28 @@ countToTen(++n);
 // code here
 
 // when you code is ready, un-comment the next line and run the file
-// console.log(countToTen());
+ console.log(countToTen());
 /* ================ Next Problem ================= */
 
 // Problem 2:
 
-const factorial = n => {
-  let result = 1;
-  for (let i = 2; i <= n; i++) {
-    result *= i;
-  }
-  return result;
-};
-
-console.log(factorial(5));
+//const factorial = n => {
+//  let result = 1;
+//  for (let i = 2; i <= n; i++) {
+//     result *= i;
+//   }
+//   return result;
+// };
+//
+// console.log(factorial(5));
 
 // write the above function in a recursive way.
+const recursiveFactorial = n => {
+  let result = 1;
+  if (n === 0) return;
+  result = n * recursiveFactorial(n - 1);
+
+}
 
 // when your code is ready, un-comment the next line and run the file
 // console.log(recursiveFactorial());
